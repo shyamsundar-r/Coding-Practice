2d7c38d38a19ec7c0c2255aedcf6004b2b96e043
SHYAM SUNDAR R <rshyamsundar.in@gmail.com>
2025-09-19 21:42:28 +0530

    Update RightAngle-Triangle.java

--- RightAngle-Triangle.java
+++ RightAngle-Triangle.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+import java.io.*;
 class Main {
     public static void main(String[] args) {
         System.out.println("Upward Triangle");
-        int n = 5;
+        System.out.println("Inverted Triangle");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number of Rows:");
+        int n = sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("*");
