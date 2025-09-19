cf6349d2395f435adf07304d22b84eb28bf88062
SHYAM SUNDAR R <rshyamsundar.in@gmail.com>
2025-09-19 21:36:14 +0530

    Update Inverted-Triangle.java

--- Inverted-Triangle.java
+++ Inverted-Triangle.java
@@ -1,7 +1,11 @@
+import java.util.*;
+import java.io.*;
 class Main {
     public static void main(String[] args) {
         System.out.println("Inverted Triangle");
-        int n = 3;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number of Rows:");
+        int n = sc.nextInt();
         for(int i=n;i>=1;i--){
             for(int j=0;j<n-i;j++){
                 System.out.print(" ");
