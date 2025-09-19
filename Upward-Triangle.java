586a82e1c1da9efd43caf461cfa8451afaac9ee8
SHYAM SUNDAR R <rshyamsundar.in@gmail.com>
2025-09-19 21:19:53 +0530

    Create Upward-Triangle.java

---
+++ Upward-Triangle.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Upward Triangle");
+        int n = 5;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
