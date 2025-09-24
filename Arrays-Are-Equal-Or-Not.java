27b76b3297add0f3e6cba25471728668f6dc3859
SHYAM SUNDAR R <rshyamsundar.in@gmail.com>
2025-09-24 22:00:18 +0530

    Create Arrays-Are-Equal-Or-Not.java

---
+++ Arrays-Are-Equal-Or-Not.java
@@ -0,0 +1,25 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Arrays Are Equal Or Not");
+        int[] a = {2,4,6,8,10};
+        int[] b = {1,3,5,7,9};
+        boolean res = true;
+        if(a.length != b.length){
+            res = false;
+        }
+        else{
+            for(int i=0;i<a.length;i++){
+                if(a[i] != b[i]){
+                    res = false;
+                    break;
+                }
+            }
+        }
+        if(res){
+            System.out.print("Arrays are Equal");
+        }
+        else{
+            System.out.print("Arrays are not Equal");
+        }
+    }
+}
